6b3ad238f78e9d88da816a181ecd0087f9d394a9
youngsoosoo <lovepys01@naver.com>
2023-08-25 15:39:45 +0900

    Persistence Layer 테스트

--- cafekiosk/src/main/java/sample/cafekiosk/spring/CafekioskApplication.java
+++ cafekiosk/src/main/java/sample/cafekiosk/spring/CafekioskApplication.java
@@ -1,9 +1,11 @@
-package sample.cafekiosk;
+package sample.cafekiosk.spring;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class CafekioskApplication {
 
     public static void main(String[] args) {
