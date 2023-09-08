c5ec0587f2ac1eac477e82ec0a380a7cd051b08a
youngsoosoo <lovepys01@naver.com>
2023-09-08 23:22:17 +0900

    Business Layer 테스트 - 2, 같은 주문에 대한 예외 사항 리팩토링

--- cafekiosk/src/main/java/sample/cafekiosk/spring/api/service/order/OrderService.java
+++ cafekiosk/src/main/java/sample/cafekiosk/spring/api/service/order/OrderService.java
@@ -2,6 +2,8 @@ package sample.cafekiosk.spring.api.service.order;
 
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 import org.springframework.stereotype.Service;
 
@@ -23,13 +25,24 @@ public class OrderService {
     public OrderResponse createOrder(OrderCreateRequest request, LocalDateTime registeredDateTime) {
 
         List<String> productNumbers = request.getProductNumbers();
-        // Product
-        List<Product> products = productRepository.findAllByProductNumberIn(productNumbers);
 
-        Order order = Order.create(products, registeredDateTime);
+        List<Product> duplicateProducts = findProductBy(productNumbers);
+
+        Order order = Order.create(duplicateProducts, registeredDateTime);
         Order savedOrder = orderRepository.save(order);
 
         // Order
         return OrderResponse.of(savedOrder);
+    }
+
+    private List<Product> findProductBy(List<String> productNumbers) {
+        // Product
+        List<Product> products = productRepository.findAllByProductNumberIn(productNumbers);
+        Map<String, Product> productMap = products.stream()
+            .collect(Collectors.toMap(Product::getProductNumber, p -> p));
+
+        return productNumbers.stream()
+            .map(productMap::get)
+            .collect(Collectors.toList());
     }
 }
