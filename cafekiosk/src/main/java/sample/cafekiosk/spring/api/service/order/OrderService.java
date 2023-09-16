19a2f504d714b112982f8471713364f1ce184d21
youngsoosoo <lovepys01@naver.com>
2023-09-16 21:46:16 +0900

    Business Layer 테스트 - 4, 재고 감소에는 동시성 고민이 필요하다.

--- cafekiosk/src/main/java/sample/cafekiosk/spring/api/service/order/OrderService.java
+++ cafekiosk/src/main/java/sample/cafekiosk/spring/api/service/order/OrderService.java
@@ -1,14 +1,8 @@
 package sample.cafekiosk.spring.api.service.order;
 
-import java.time.LocalDateTime;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Map;
-import java.util.stream.Collectors;
-
-import org.springframework.stereotype.Service;
-
 import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import sample.cafekiosk.spring.api.controller.order.request.OrderCreateRequest;
 import sample.cafekiosk.spring.api.service.order.response.OrderResponse;
 import sample.cafekiosk.spring.domain.order.Order;
@@ -19,6 +13,13 @@ import sample.cafekiosk.spring.domain.product.ProductType;
 import sample.cafekiosk.spring.domain.stock.Stock;
 import sample.cafekiosk.spring.domain.stock.StockRepository;
 
+import java.time.LocalDateTime;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+@Transactional
 @RequiredArgsConstructor
 @Service
 public class OrderService {
@@ -27,43 +28,39 @@ public class OrderService {
     private final OrderRepository orderRepository;
     private final StockRepository stockRepository;
 
+    /**
+     * 재고 감소 -> 동시성 고민
+     */
+
     public OrderResponse createOrder(OrderCreateRequest request, LocalDateTime registeredDateTime) {
-
         List<String> productNumbers = request.getProductNumbers();
-        List<Product> products = findProductBy(productNumbers);
+        List<Product> products = findProductsBy(productNumbers);
 
-        // 재고 차감 체크가 필요한 상품들 filter
-        List<String> stockProductNumbers = products.stream()
-            .filter(product -> ProductType.containsStockType(product.getType()))
-            .map(Product::getProductNumber)
-            .collect(Collectors.toList());
-        // 재고 엔티티 조회
-        List<Stock> stocks = stockRepository.findAllByProductNumberIn(stockProductNumbers);
-        Map<String, Stock> stockMap = stocks.stream()
-            .collect(Collectors.toMap(Stock::getProductNumber, s->s));
+        deductStockQuantities(products);
 
-        // 상품별 counting
-        Map<String, Long> productCountingMap = stockProductNumbers.stream()
-            .collect(Collectors.groupingBy(p -> p, Collectors.counting()));
+        Order order = Order.create(products, registeredDateTime);
+        Order savedOrder = orderRepository.save(order);
+        return OrderResponse.of(savedOrder);
+    }
 
-        // 재고 차감 시도
+    private void deductStockQuantities(List<Product> products) {
+        List<String> stockProductNumbers = extractStockProductNumbers(products);
+
+        Map<String, Stock> stockMap = createStockMapBy(stockProductNumbers);
+        Map<String, Long> productCountingMap = createCountingMapBy(stockProductNumbers);
+
         for (String stockProductNumber : new HashSet<>(stockProductNumbers)) {
             Stock stock = stockMap.get(stockProductNumber);
             int quantity = productCountingMap.get(stockProductNumber).intValue();
-            if(stock.isQuantityLessThan(quantity)){
+
+            if (stock.isQuantityLessThan(quantity)) {
                 throw new IllegalArgumentException("재고가 부족한 상품이 있습니다.");
             }
             stock.deductQuantity(quantity);
         }
-
-        Order order = Order.create(products, registeredDateTime);
-        Order savedOrder = orderRepository.save(order);
-        // Order
-        return OrderResponse.of(savedOrder);
     }
 
-    private List<Product> findProductBy(List<String> productNumbers) {
-        // Product
+    private List<Product> findProductsBy(List<String> productNumbers) {
         List<Product> products = productRepository.findAllByProductNumberIn(productNumbers);
         Map<String, Product> productMap = products.stream()
             .collect(Collectors.toMap(Product::getProductNumber, p -> p));
@@ -71,5 +68,23 @@ public class OrderService {
         return productNumbers.stream()
             .map(productMap::get)
             .collect(Collectors.toList());
+    }
+
+    private static List<String> extractStockProductNumbers(List<Product> products) {
+        return products.stream()
+            .filter(product -> ProductType.containsStockType(product.getType()))
+            .map(Product::getProductNumber)
+            .collect(Collectors.toList());
+    }
+
+    private Map<String, Stock> createStockMapBy(List<String> stockProductNumbers) {
+        List<Stock> stocks = stockRepository.findAllByProductNumberIn(stockProductNumbers);
+        return stocks.stream()
+            .collect(Collectors.toMap(Stock::getProductNumber, s -> s));
+    }
+
+    private static Map<String, Long> createCountingMapBy(List<String> stockProductNumbers) {
+        return stockProductNumbers.stream()
+            .collect(Collectors.groupingBy(p -> p, Collectors.counting()));
     }
 }
--- cafekiosk/src/test/java/sample/cafekiosk/spring/api/service/order/OrderServiceTest.java
+++ cafekiosk/src/test/java/sample/cafekiosk/spring/api/service/order/OrderServiceTest.java
@@ -26,7 +26,6 @@ import sample.cafekiosk.spring.domain.stock.StockRepository;
 
 @ActiveProfiles("test")
 @SpringBootTest
-// @DataJpaTest
 class OrderServiceTest {
 
     @Autowired
@@ -44,18 +43,79 @@ class OrderServiceTest {
     @Autowired
     private OrderService orderService;
 
-    // @AfterEach
-    // void tearDown() {
-    //     // productRepository.deleteAll();
-    //     orderProductRepository.deleteAllInBatch();
-    //     productRepository.deleteAllInBatch();
-    //     orderRepository.deleteAllInBatch();
-    // }
+    @AfterEach
+    void tearDown() {
+        orderProductRepository.deleteAllInBatch();
+        productRepository.deleteAllInBatch();
+        orderRepository.deleteAllInBatch();
+        stockRepository.deleteAllInBatch();
+    }
+
+    @DisplayName("주문번호 리스트를 받아 주문을 생성한다.")
+    @Test
+    void createOrder() {
+        // given
+        LocalDateTime registeredDateTime = LocalDateTime.now();
+
+        Product product1 = createProduct(HANDMADE, "001", 1000);
+        Product product2 = createProduct(HANDMADE, "002", 3000);
+        Product product3 = createProduct(HANDMADE, "003", 5000);
+        productRepository.saveAll(List.of(product1, product2, product3));
+
+        OrderCreateRequest request = OrderCreateRequest.builder()
+            .productNumbers(List.of("001", "002"))
+            .build();
+
+        // when
+        OrderResponse orderResponse = orderService.createOrder(request, registeredDateTime);
+
+        // then
+        assertThat(orderResponse.getId()).isNotNull();
+        assertThat(orderResponse)
+            .extracting("registeredDateTime", "totalPrice")
+            .contains(registeredDateTime, 4000);
+        assertThat(orderResponse.getProducts()).hasSize(2)
+            .extracting("productNumber", "price")
+            .containsExactlyInAnyOrder(
+                tuple("001", 1000),
+                tuple("002", 3000)
+            );
+    }
+
+    @DisplayName("중복되는 상품번호 리스트로 주문을 생성할 수 있다.")
+    @Test
+    void createOrderWithDuplicateProductNumbers() {
+        // given
+        LocalDateTime registeredDateTime = LocalDateTime.now();
+
+        Product product1 = createProduct(HANDMADE, "001", 1000);
+        Product product2 = createProduct(HANDMADE, "002", 3000);
+        Product product3 = createProduct(HANDMADE, "003", 5000);
+        productRepository.saveAll(List.of(product1, product2, product3));
+
+        OrderCreateRequest request = OrderCreateRequest.builder()
+            .productNumbers(List.of("001", "001"))
+            .build();
+
+        // when
+        OrderResponse orderResponse = orderService.createOrder(request, registeredDateTime);
+
+        // then
+        assertThat(orderResponse.getId()).isNotNull();
+        assertThat(orderResponse)
+            .extracting("registeredDateTime", "totalPrice")
+            .contains(registeredDateTime, 2000);
+        assertThat(orderResponse.getProducts()).hasSize(2)
+            .extracting("productNumber", "price")
+            .containsExactlyInAnyOrder(
+                tuple("001", 1000),
+                tuple("001", 1000)
+            );
+    }
 
     @DisplayName("재고와 관련된 상품이 포함되어 있는 주문번호 리스트를 받아 주문을 생성한다.")
     @Test
     void createOrderWithStock() {
-
         // given
         LocalDateTime registeredDateTime = LocalDateTime.now();
 
@@ -96,73 +156,36 @@ class OrderServiceTest {
                 tuple("001", 0),
                 tuple("002", 1)
             );
-     }
+    }
 
-    @DisplayName("주문번호 리스트를 받아 주문을 생성한다.")
+    @DisplayName("재고가 부족한 상품으로 주문을 생성하려는 경우 예외가 발생한다.")
     @Test
-    void createOrder() {
-
+    void createOrderWithNoStock() {
         // given
         LocalDateTime registeredDateTime = LocalDateTime.now();
 
-        Product product1 = createProduct(HANDMADE, "001", 1000);
-        Product product2 = createProduct(HANDMADE, "002", 3000);
+        Product product1 = createProduct(BOTTLE, "001", 1000);
+        Product product2 = createProduct(BAKERY, "002", 3000);
         Product product3 = createProduct(HANDMADE, "003", 5000);
         productRepository.saveAll(List.of(product1, product2, product3));
 
+        Stock stock1 = Stock.create("001", 2);
+        Stock stock2 = Stock.create("002", 2);
+        stock1.deductQuantity(1); // todo
+        stockRepository.saveAll(List.of(stock1, stock2));
+
         OrderCreateRequest request = OrderCreateRequest.builder()
-            .productNumbers(List.of("001", "002"))
+            .productNumbers(List.of("001", "001", "002", "003"))
             .build();
 
-        // when
-        OrderResponse orderResponse = orderService.createOrder(request, registeredDateTime);
-
-        // then
-        assertThat(orderResponse.getId()).isNotNull();
-        assertThat(orderResponse)
-            .extracting("registeredDateTime", "totalPrice")
-            .contains(registeredDateTime, 4000);
-        assertThat(orderResponse.getProducts()).hasSize(2)
-            .extracting("productNumber", "price")
-            .containsExactlyInAnyOrder(
-                tuple("001", 1000),
-                tuple("002", 3000)
-            );
+        // when // then
+        assertThatThrownBy(() -> orderService.createOrder(request, registeredDateTime))
+            .isInstanceOf(IllegalArgumentException.class)
+            .hasMessage("재고가 부족한 상품이 있습니다.");
     }
 
-     @DisplayName("중복되는 상품번호 리스트로 주문을 생성할 수 있다.")
-     @Test
-     void createOrderWithDuplicateProductNumbers() {
 
-         // given
-         LocalDateTime registeredDateTime = LocalDateTime.now();
-
-         Product product1 = createProduct(HANDMADE, "001", 1000);
-         Product product2 = createProduct(HANDMADE, "002", 3000);
-         Product product3 = createProduct(HANDMADE, "003", 5000);
-         productRepository.saveAll(List.of(product1, product2, product3));
-
-         OrderCreateRequest request = OrderCreateRequest.builder()
-             .productNumbers(List.of("001", "001"))
-             .build();
-
-         // when
-         OrderResponse orderResponse = orderService.createOrder(request, registeredDateTime);
-
-         // then
-         assertThat(orderResponse.getId()).isNotNull();
-         assertThat(orderResponse)
-             .extracting("registeredDateTime", "totalPrice")
-             .contains(registeredDateTime, 2000);
-         assertThat(orderResponse.getProducts()).hasSize(2)
-             .extracting("productNumber", "price")
-             .containsExactlyInAnyOrder(
-                 tuple("001", 1000),
-                 tuple("001", 1000)
-             );
-      }
-
-     private Product createProduct(ProductType type, String productNumber, int price){
+    private Product createProduct(ProductType type, String productNumber, int price) {
         return Product.builder()
             .type(type)
             .productNumber(productNumber)
@@ -170,5 +193,5 @@ class OrderServiceTest {
             .sellingStatus(SELLING)
             .name("메뉴 이름")
             .build();
-     }
+    }
 }
